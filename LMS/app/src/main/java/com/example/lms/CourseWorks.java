d22fe0b30946a5114db7e6103417b159a3472d2e
ravindu0823 <guestpc87@gmail.com>
2023-05-18 04:41:23 +0530

    Created Backend for coursework side

--- LMS/app/src/main/java/com/example/lms/CourseWorks.java
+++ LMS/app/src/main/java/com/example/lms/CourseWorks.java
@@ -1,26 +1,98 @@
 package com.example.lms;
 
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class CourseWorks extends Fragment {
 
+    TextView textView, txtDueDate;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://learning-management-syst-24c6c-default-rtdb.firebaseio.com/");
+    Button btnDownload;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_cource_work2, container, false);
 
+        textView = view.findViewById(R.id.txtFinalReportSubmission);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UploadCourseWorks uploadCourseWorks = new UploadCourseWorks();
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, uploadCourseWorks).commit();
+            }
+        });
+
+        txtDueDate = view.findViewById(R.id.txtDueDate);
+        btnDownload = view.findViewById(R.id.btnDownloadCourseWork);
+
+        SessionManager sessionManager = new SessionManager(getContext());
+        String moduleName = sessionManager.getModuleName();
+        String degree = sessionManager.getDegree();
+
+        Toast.makeText(getContext(), moduleName + " " + degree, Toast.LENGTH_SHORT).show();
+
+        databaseReference.child("Module").child(degree).child(moduleName).child("coursework").child("final").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                txtDueDate.setText("Due Date: " + snapshot.child("due_date").getValue().toString());
+
+                btnDownload.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        downloadFile(snapshot.child("file_path").getValue().toString());
+                    }
+                });
+
+                // Log.d("TAG", "onDataChange: " + snapshot.child("file_path").getValue().toString());
 
 
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         return view;
+    }
+
+    private void downloadFile(String url) {
+        downloadFiles(getContext(), "myFile", ".pdf", DIRECTORY_DOWNLOADS, url);
+    }
+
+    public void downloadFiles(Context context, String fileName, String fileExtension, String destinationDirectory, String url) {
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName + fileExtension);
+
+        downloadManager.enqueue(request);
     }
 }
--- LMS/app/src/main/java/com/example/lms/UploadCourseWorks.java
+++ LMS/app/src/main/java/com/example/lms/UploadCourseWorks.java
@@ -1,20 +1,93 @@
 package com.example.lms;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 
 public class UploadCourseWorks extends Fragment {
-
+    RelativeLayout relativeLayout;
+    Button upload_btn;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://learning-management-syst-24c6c-default-rtdb.firebaseio.com/");
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageReference = storage.getReferenceFromUrl("gs://learning-management-syst-24c6c.appspot.com/");
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_courcework, container, false);
+        View view = inflater.inflate(R.layout.fragment_courcework, container, false);
+
+        relativeLayout = view.findViewById(R.id.relativeLayoutFileUpload);
+        upload_btn = view.findViewById(R.id.btnSubmit);
+
+        return view;
+    }
+
+    private void selectFiles() {
+        Intent intent = new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select PDF Files..."), 1);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            UploadFiles(data.getData());
+        }
+    }
+
+    private void UploadFiles(Uri data) {
+        final ProgressDialog progressDialog = new ProgressDialog(getContext());
+        progressDialog.setTitle("Uploading......");
+
+        StorageReference reference = storageReference.child("Upload/" + System.currentTimeMillis() + ".pdf");
+        reference.putFile(data).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                while (!uriTask.isComplete()) ;
+                Uri url = uriTask.getResult();
+
+                /*PDFClass pdfClass = new PDFClass(pdf_name.getText().toString(), url.toString());
+                databaseReference.child("Software_Engineering").child("Artificial_Intelligence").child("Materials").child(databaseReference.push().getKey()).setValue(pdfClass);*/
+                // TODO: THAWA HADANNA THIYENAWAAAAAAA
+
+                Toast.makeText(getContext(), "File Uploded!!", Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                double progress = (100.0 * snapshot.getBytesTransferred()) / snapshot.getTotalByteCount();
+                progressDialog.setMessage("Uploaded: " + (int) progress + "%");
+            }
+        });
     }
 }
